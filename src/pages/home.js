f9fc5decac6fcc2ba5bbeeed3c70330c381e3cbe
imDangerous <imflower80@gmail.com>
2022-05-23 02:37:06 +0900

    added

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Navigation />
+      <p>This is the homepage</p>
+    </div>
+  );
+};
+
+export default Home;
